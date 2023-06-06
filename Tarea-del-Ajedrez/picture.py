7e5dc56d4f105d00cc80b2dc5a7030c67b774519
Elias Vizcarra <evizcarraga@unsa.edu.pe>
2023-06-05 19:48:53 -0500

    Completado horizontalRepeat()

--- Tarea-del-Ajedrez/picture.py
+++ Tarea-del-Ajedrez/picture.py
@@ -52,7 +52,10 @@ class Picture:
   def horizontalRepeat(self, n):
     """ Devuelve una nueva figura repitiendo la figura actual al costado
         la cantidad de veces que indique el valor de n """
-    return Picture(None)
+    hrepeat = []
+    for i in range(len(self.img)):
+      hrepeat.append(self.img[i] * (n + 1))
+    return hrepeat
 
   def verticalRepeat(self, n):
     return Picture(None)
